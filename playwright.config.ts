b7f2bad899c3f4800da01f509bff7c4bb0d73aff
Pablo Garcia Moralez <74436088+pablogarciamoralez@users.noreply.github.com>
2026-06-29 13:42:14 -0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,10 +17,10 @@ export default defineConfig({
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Enable 1 retry for local/CI runs to handle sandbox site flakiness */
-  retries: 2,
-  /* Opt out of parallel tests on CI. */
-  workers: 1,
+  /* Enable 3 retries on CI, 2 locally to handle sandbox site flakiness */
+  retries: process.env.CI ? 3 : 2,
+  /* Use 2 parallel workers on CI, 1 locally. */
+  workers: process.env.CI ? 2 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
